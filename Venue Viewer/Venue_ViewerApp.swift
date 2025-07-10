5d2c115475df4ca4eac4cde2471a31121d85b3cf
Imo Madjid <mhmmadjid@gmail.com>
2025-07-10 10:17:25 +0700

    Initial Commit

---
+++ Venue Viewer/Venue_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Venue_ViewerApp.swift
+//  Venue Viewer
+//
+//  Created by Imo Madjid on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Venue_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
